2346f4c8da71a406a9246efa793d3ffb1100cdd2
inuter <inuter@qq.com>
2020-11-01 14:15:55 +0800

    configure plugins

---
+++ configure-plugins/src/main/java/com/example/Util.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * @author inuter@qq.com
+ * @date 2020/11/01
+ */
+public class Util {
+
+    /**
+     * Print "Hello world"
+     */
+    public static void sayHelloWorld() {
+        System.out.println("Hello world!");
+    }
+}
