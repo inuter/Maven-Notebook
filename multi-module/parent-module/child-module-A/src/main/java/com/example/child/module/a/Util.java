d8eaa935422218ecd6ee2da689f86db1c515e18e
inuter <inuter@qq.com>
2020-11-04 15:10:53 +0800

    multi-module

---
+++ multi-module/parent-module/child-module-A/src/main/java/com/example/child/module/a/Util.java
@@ -0,0 +1,15 @@
+package com.example.child.module.a;
+
+/**
+ * @author inuter@qq.com
+ * @date 2020/11/04
+ */
+public class Util {
+
+    /**
+     * print "Hello world!"
+     */
+    public static void sayHelloWorld() {
+        System.out.println("Hello world!");
+    }
+}
---
+++ multi-module/parent-module/child-module-B/src/main/java/com/example/child/module/b/InvokeMethod.java
@@ -0,0 +1,17 @@
+package com.example.child.module.b;
+
+import com.example.child.module.a.Util;
+
+/**
+ * @author inuter@qq.com
+ * @date 2020/11/04
+ */
+public class InvokeMethod {
+
+    /**
+     * invoke sayHelloWorld method
+     */
+    public static void invokeHelloWorld() {
+        Util.sayHelloWorld();
+    }
+}
